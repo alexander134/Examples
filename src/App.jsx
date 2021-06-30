fbafbf9946f73f3b094e8b68caefd7090668c770
alexander134 <alexanderdiaz_134@hotmail.com>
2021-06-30 16:28:40 -0400

    finalizacion del proyecto con deploy npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import HomeWorksWeeks from './FhomeWorkWeeks/HomeWorksWeeks2'//for components
 function App() {
   return (
     <div>
+      <div className="col-sm-12">
+        <h2 className="text-center text-info">Tareas Asignadas</h2>
+      </div>
       <HomeWorksWeeks />
     </div>
   )
